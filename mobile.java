32c455527c8d20c5812672b3a66e84ff7214f458
Dipakmali2909 <154011279+Dipakmali2909@users.noreply.github.com>
2023-12-16 22:30:58 +0530

    Add files via upload

---
+++ mobile.java
@@ -0,0 +1,24 @@
+package oneplus;
+
+public class mobile  {
+
+	String brandname;
+    String color;
+	int ram;
+	int rom;
+	double price;
+
+	
+	 void on() {
+		System.out.println("on");
+	}
+	void about() {
+		System.out.println("brandname"+" = "+ brandname);
+		System.out.println("ram"+" = "+ram);
+		System.out.println("rom"+" = "+rom);
+		System.out.println("color"+" = "+color);
+		System.out.println("price"+" = "+price);
+
+	}
+
+}
---
+++ testmobile.java
@@ -0,0 +1,38 @@
+package oneplus;
+
+public class testmobile  {
+	
+	public static void main(String[] args) {
+	
+	 	mobile m1=new mobile();
+	 	
+	 	
+	 	System.out.println("welcome to dipak");
+	 	System.out.println("              ");
+	 	
+		m1.brandname="one plus9pro";
+	 
+	    m1.color="black";
+		m1.ram=6;
+		m1.rom=120;
+		m1.price=35000.0;
+		m1.about();
+	 	
+		System.out.println("=======================");
+
+	
+			
+			 	mobile m2=new mobile();
+			 	
+				m2.brandname="one plus10pro";
+			    m2.color="red";
+				m2.ram=6;
+				m2.rom=128;
+				m2.price=45000.0;
+				m2.about();
+				System.out.println("               ");
+			 	System.out.println("Thank you!!!");
+
+	}
+}
+
